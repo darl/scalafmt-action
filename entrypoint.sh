4c3e5250392783e3c78ba104c00c90e2c98c61bb
Vladislav Dolbilov <darl@yandex-team.ru>
2021-01-12 01:16:49 +0300

    more debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,16 @@
 #!/bin/sh -l
-set -e
+set -ex
 
 if [ -n "${GITHUB_WORKSPACE}" ] ; then
   cd "${GITHUB_WORKSPACE}/${INPUT_WORKDIR}" || exit
 fi
 
+echo "Base: $GITHUB_BASE_REF"
 echo "Current dir: $(pwd)"
 echo "Current dir listing:\n$(ls -la)"
 
 /scalafmt --mode "${INPUT_MODE}" --diff-branch "${GITHUB_BASE_REF}" --config "${INPUT_CONFIG}" .
 
 git status
+
+git diff "${GITHUB_BASE_REF}"

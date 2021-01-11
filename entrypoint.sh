8c193971bc6a99ffffc01eb2c69cf5f6b1f0c796
Vladislav Dolbilov <darl@yandex-team.ru>
2021-01-12 01:06:53 +0300

    add debug logging

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,9 @@ if [ -n "${GITHUB_WORKSPACE}" ] ; then
   cd "${GITHUB_WORKSPACE}/${INPUT_WORKDIR}" || exit
 fi
 
+echo "Current dir: ${pwd}"
+echo "Current dir listing: ${ls -la}"
+
 /scalafmt --mode "${INPUT_MODE}" --config "${INPUT_CONFIG}" .
+
+git status
